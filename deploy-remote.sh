09eebd5c9484bee39fa5c83a1f8d20ada6b6802d
makdon <makdon@makdon.me>
2021-08-08 23:14:49 +0800

    build script

--- deploy-remote.sh
+++ deploy-remote.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 # used in server
 git pull
 lsof -ti tcp:80 | xargs kill
-nohup hexo server -p 80 &
+nohup hexo server -p 80 > /dev/null 2>&1 &
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 # trigger tencent cloud deploy
 
-ssh root@43.129.216.180 "cd /personal_hexo_blog ; ./deploy-remote.sh"
+ssh root@43.129.216.180 "cd ~/personal_hexo_blog && nohup ./deploy-remote.sh &"
